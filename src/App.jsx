e14c3709921c5dea3b0c2b1e839714e872d33c81
huseyintopgul <huseyintopgul1@gmail.com>
2023-06-04 23:48:22 +0300

    React Redux App created.

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        deneme
+      </div>
+      <Routes>
+        <Route path='' element />
+        deneme
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/products/ProductsComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductsComponent = () => {
+    return (
+        <div>
+            Alışveriş Listesi
+        </div>
+    )
+}
+
+export default ProductsComponent
